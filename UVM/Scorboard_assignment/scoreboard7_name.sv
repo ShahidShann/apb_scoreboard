b0484266c3a7aca78d16a0e1d8fd0caea177421c
Meenal <114145036+meenalpannase@users.noreply.github.com>
2024-05-22 15:39:01 +0530

    Create scoreboard7_name.sv

---
+++ UVM/Scorboard_assignment/scoreboard7_name.sv
@@ -0,0 +1,9 @@
+class scoreboard extends uvm_scoreboard
+  //factory registration
+  //declaration of analysis_imp_ports from monitor/refrennce model
+  //constructor
+  //build phase
+  //connect phase
+  //run phase 
+  //write scorebrd check here with expected and actual data
+endclass
